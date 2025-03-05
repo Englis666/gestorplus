c985070a23eec98a71a7699466ea5439615220df
Englis666 <eabarros2610@gmail.com>
2025-03-05 15:56:41 -0500

    Fix: Minimos cambios

--- frontendNative-app/src/screens/CargosScreen.jsx
+++ frontendNative-app/src/screens/CargosScreen.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-
+import TablaCargos from "../componentsClosed/TablaCargos";
 
 const Cargos = () => {
-
     return (
         <View style={styles.container}>
             <View style={styles.content}>
                 <TablaCargos />
             </View>
         </View>
-
-    )
-
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 16,
+    },
+    content: {
+        flex: 1, // Asegura que el contenido ocupe todo el espacio
     },
 });
 
