873087964ab50bc7edeb14130b628336de1bcc65
Englis666 <eabarros2610@gmail.com>
2025-03-05 15:57:22 -0500

    Feature: Se realiza cambios en los estilos para correcta visualizacion

--- frontendNative-app/src/screens/EmpleadosScreen.jsx
+++ frontendNative-app/src/screens/EmpleadosScreen.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import TablaEmpleados from "../componentsClosed/TablaEmpleados";
 
 const Empleados = () => {
-
     return (
-
         <View style={styles.container}>
-            <View styl={styles.content}>
+            <View style={styles.content}>
                 <TablaEmpleados />
             </View>
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 16,
     },
+    content: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
 
 export default Empleados;
-
