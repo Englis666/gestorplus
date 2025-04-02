38520793ae2e229129a46fcc69ba1ca18cc42462
Englis666 <eabarros2610@gmail.com>
2025-04-02 16:37:04 -0500

    Fix <index.php> : Ahora se utilizan dos enpoints y con la validacion se qusa para redirigir a un endpoint especifico ( No recomendable)

--- backend/index.php
+++ backend/index.php
@@ -1,24 +1,25 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-header("Access-Control-Allow-Origin: * ");
-
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PATCH, OPTIONS");
-
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-
 header("Access-Control-Allow-Credentials: true");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit;
 }
+
 header("Content-Type: application/json; charset=UTF-8");
 
-
 require_once __DIR__ . '/vendor/autoload.php';
-require_once 'servidor/servidor.php';
+
+$endpoint = $_GET['endpoint'] ?? $_POST['endpoint'] ?? null;
+
+if ($endpoint === 'archivos') {
+    require_once 'servidor/archivos.php';
+} else {
+    require_once 'servidor/servidor.php';
+}
 
 ?>
