ee6bf4a55a76b71136e610a9ee24a62db0820fc9
Englis666 <eabarros2610@gmail.com>
2025-04-06 20:10:16 -0500

    <Fix>:

--- frontend/src/views/Ausencias.jsx
+++ frontend/src/views/Ausencias.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import NavbarClosed from "../componentsClosed/Navbar";
 import TablaAusencias from "../componentsClosed/TablaAusencias";
+import FormularioSolicitud from "../componentsClosed/form/FormularioSolicitudAusencia";
 
 const Ausencias = () => {
   return (
     <div className="bg-light min-vh-100" style={{ transition: "all 3s ease", display: "flex" }}>
-        <NavbarClosed />
+      <NavbarClosed />
       <div className="flex-grow-1 p-4" style={{ backgroundColor: "#ECF0F1" }}>
-        <TablaAusencias/>
+        <TablaAusencias />
       </div>
     </div>
   );
