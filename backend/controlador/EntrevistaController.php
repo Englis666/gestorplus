46c390819fb81fd00c103a924cac22d94c471b57
Englis666 <eabarros2610@gmail.com>
2025-04-19 18:42:32 -0500

    Feature: <EntrevistaController.php> : Se realiza el controlador de empleado para respetar los principios SOLID

---
+++ backend/controlador/EntrevistaController.php
@@ -0,0 +1,59 @@
+<?php
+namespace Controlador;
+
+use Modelo\Entrevista;
+use Servicio\JsonResponseService;
+use Servicio\TokenService;
+
+class EntrevistaController{
+    private Entrevista $entrevista; 
+    private ?\PDO $db;
+    private JsonResponseService $jsonResponseService;
+    private TokenService $tokenService;
+
+    public function __construct(){
+        $this->db = (new \Config\Database())->getConnection();
+        $this->entrevista = new Entrevista($this->db);
+        $this->tokenService = new TokenService();
+        $this->jsonResponseService = new jsonResponseService();
+    }
+
+    public function responder(array $data , int $httpCode = 200): void{
+        $this->jsonResponseService->responder($data, $httpCode);
+    }
+
+    private function verificarDatosRequeridos(array $data, array $camposRequeridos): bool{
+        foreach ($camposRequeridos as $campo){
+            if (!isset($data[$campo])){
+                $this->responder(['error' => "Falta el campo requerido: $campo"], 400);
+                return false;
+            }
+        }
+    return true;
+    }
+
+    public function asignarEntrevista(array $data)
+    {
+        $required = ['fecha', 'hora', 'lugarMedio'];
+        if (!$this->verificarDatosRequeridos($data, $required)) {
+            return;
+        }
+        $resultado = $this->entrevista->asignarEntrevista($data);
+        $this->responder(['Entrevista' => $resultado]);
+    }
+
+    public function obtenerEntrevistas()
+    {
+        $this->responder(['Entrevista' => $this->entrevista->obtenerEntrevistas()]);
+    }
+
+    
+    public function obtenerDatosDelEntrevistado(?string $num_doc = null)
+    {
+        $num_doc = $_GET['num_doc'] ?? $num_doc;
+        $entrevistado = $this->entrevista->obtenerDatosDelEntrevistado($num_doc);
+        $this->responder(["Entrevistado" => $entrevistado ?: ["error" => "No se encontraron datos para el documento proporcionado"]]);
+    }
+
+
+}
