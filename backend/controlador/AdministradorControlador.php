7080fb51dcd2aae5bfb8de45d187f216429323d5
Englis666 <eabarros2610@gmail.com>
2025-04-19 20:38:31 -0500

    Eliminacion de pre ultima funcion

--- backend/controlador/AdministradorControlador.php
+++ backend/controlador/AdministradorControlador.php
@@ -1,10 +1,7 @@
 <!--<?php
 
     //Para el controladorEmpleados
-    //public function obtenerEmpleados()
-    //{
-      //  $this->responder(['empleados' => $this->administrador->obtenerEmpleados()]);
-    //}
+    
 
     //Para el controlador usuario
     //public function obtenerUsuarios()
