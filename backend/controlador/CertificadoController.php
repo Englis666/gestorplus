f58422952030aab3f62aab483a436afe800387c7
Englis666 <eabarros2610@gmail.com>
2025-04-20 19:48:50 -0500

    Cambio en el controlador

--- backend/controlador/CertificadoController.php
+++ backend/controlador/CertificadoController.php
@@ -1,44 +1,36 @@
 <?php 
+
+declare(strict_types = 1);
+
 namespace Controlador;
 
+use Core\Controller\BaseController;
 use Modelo\Certificado;
-use Servicio\JsonResponseService;
 use Servicio\TokenService;
+use PDO;
+use Exception;
 
-class CertificadoController{
-    private $db;
+class CertificadoController extends BaseController{
+    private PDO $db;
     private Certificado $certificado;
-    private JsonResponseService $jsonResponseService;
     private TokenService $tokenService;
 
    public function __construct()
     {
+        parent::__construct();
         $this->db = (new \Config\Database())->getConnection();
         $this->certificado = New certificado($this->db);
         $this->tokenService = new TokenService();
-        $this->jsonResponseService = new JsonResponseService();
-    }
-
-    public function responder(array $data, int $httpCode = 200): void{
-        $this->jsonResponseService->responder($data, $httpCode);
-    }
-
-    private function verificarDatosRequeridos(array $data, array $camposRequeridos): bool
-    {
-        foreach ($camposRequeridos as $campo) {
-            if (!isset($data[$campo])) {
-                $this->responder(['error' => "Falta el campo requerido: $campo"], 400);
-                return false;
-            }
-        }
-        return true;
     }
 
     public function obtenerDatosParaCertificado(){
-        $decoded = $this->verificarToken();
-        $resultado = $this->usuario->obtenerDatosParaCertificado($decoded->data->num_doc);
-        $this->jsonResponse('success', '' , ['Certificado' => $resultado ?: []]); 
-        
+        try{
+         $num_doc = $this->tokenService->validarToken();
+         $certificado = $this->certificado->obtenerDatosParaCertificado($num_doc);
+         $this->jsonResponseService->responder(['Certificado' => $certificado]);
+        } catch (Exception $e){
+            $this->jsonResponseService->responderError($e->getMessage(), $e->getCode() ?: 400);
+        }
     }
 
 
