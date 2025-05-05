d32134f9b72ce0e3b0d8e955152a29e000a1f920
Englis666 <eabarros2610@gmail.com>
2025-05-05 17:28:48 -0500

    Cambios

--- backend/migrations/MigrarExcelRunner.php
+++ backend/migrations/MigrarExcelRunner.php
@@ -1,14 +1,31 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
+
+require_once __DIR__ . '../vendor/autoload.php';
 require_once __DIR__ . '/MigrarExcel.php';
 
 use Migrations\MigrarExcel;
 
 if ($argc < 2) {
-    echo "Uso: php MigrarExcelRunner.php rutaquedaelbash";
+    echo "Uso: php MigrarExcelRunner.php ruta_del_archivo\n";
     exit(1);
 }
 
 $filename = $argv[1];
-$migrador = new MigrarExcel();
-$migrador->importarExcel($filename);
+
+// Verificar si el archivo existe
+if (!file_exists($filename)) {
+    echo "Error: El archivo '$filename' no existe.\n";
+    exit(1);
+}
+
+try {
+    // Crear una instancia de MigrarExcel
+    $migrador = new MigrarExcel();
+    
+    // Llamar al método importarExcel para procesar el archivo
+    $migrador->importarExcel($filename);
+} catch (Exception $e) {
+    // Capturar cualquier excepción que ocurra durante la migración
+    echo "Se produjo un error al realizar la migración: " . $e->getMessage() . "\n";
+    exit(1);
+}
