628878514bd827e0a92d93a9c1cc43ec9e1ceb49
stemansote <hearthismusic808@gmail.com>
2025-05-06 14:24:21 -0500

    actualizar el instalador

--- backend/migrations/MigrarExcelRunner.php
+++ backend/migrations/MigrarExcelRunner.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '../vendor/autoload.php';
-require_once __DIR__ . '/MigrarExcel.php';
+require_once '../vendor/autoload.php';
+require_once '/MigrarExcel.php';
 
 use Migrations\MigrarExcel;
 
