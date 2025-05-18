6398d6825c53166c9e29584ec999952856f1a80f
Englis666 <eabarros2610@gmail.com>
2025-05-18 15:48:11 -0500

    feat: Servicio de mailer con credenciales (Toca volverlo .env)

---
+++ backend/service/Mailer.php
@@ -0,0 +1,34 @@
+<?php
+namespace Service;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class Mailer {
+    public static function enviarCorreo($para, $asunto, $contenidoHtml): bool {
+        $mail = new PHPMailer(true);
+
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp-relay-offshore-southamerica-east-v2.sendinblue.com';
+            $mail->SMTPAuth = true;                 // Nota la S mayúscula
+            $mail->Username = '8d5363001@smtp-brevo.com';  // Tu usuario SMTP (email)
+            $mail->Password = 'JLANaUHRTcVs3b0t';            // La contraseña o token SMTP (nota la P mayúscula)
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+
+            $mail->setFrom('scrimsdiscord2@gmail.com', 'GestorPlus');
+            $mail->addAddress($para);
+
+            $mail->isHTML(true);
+            $mail->Subject = $asunto;
+            $mail->Body    = $contenidoHtml;
+
+            $mail->send();
+            return true;
+
+        }  catch (Exception $e) {
+            throw new \Exception('Error al enviar correo: ' . $e->getMessage());
+        }
+    }
+}
