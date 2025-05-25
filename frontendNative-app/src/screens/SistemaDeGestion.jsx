ffd9108145af10a96ef2cdd568ad0530ec7fd414
Englis666 <eabarros2610@gmail.com>
2025-05-25 17:42:19 -0500

    UPDATE: Formato y espaciado mejorados en el componente SistemaDeGestion

--- frontendNative-app/src/screens/SistemaDeGestion.jsx
+++ frontendNative-app/src/screens/SistemaDeGestion.jsx
@@ -3,15 +3,12 @@ import { View, StyleSheet } from "react-native";
 import TablaSistemaDeGestion from "../componentsClosed/TablaSistemaDeGestion";
 
 const SistemaDeGestion = () => {
-
-    return (
-        <View style={StyleSheet.container}>
-            <View style={StyleSheet.content}>
-                <TablaSistemaDeGestion />
-            </View>
-        </View>
-
-    )
-
-}
+  return (
+    <View style={StyleSheet.container}>
+      <View style={StyleSheet.content}>
+        <TablaSistemaDeGestion />
+      </View>
+    </View>
+  );
+};
 export default SistemaDeGestion;
