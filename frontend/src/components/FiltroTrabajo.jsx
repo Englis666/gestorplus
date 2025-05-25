7efbba7e4de1257fdc1c329a4b42231633788e1b
Englis666 <eabarros2610@gmail.com>
2025-05-25 17:42:29 -0500

    UPDATE: Mejora del formato y espaciado en el componente FiltroTrabajo

--- frontend/src/components/FiltroTrabajo.jsx
+++ frontend/src/components/FiltroTrabajo.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 
-const FiltroTrabajo = () =>{
-    return(
-        <div>
-        <section class="job-filter bg-light">
-            <h1 class="heading mb-4">Buscar trabajos</h1>
-            <form action="" method="post" class="p-4 bg-white border rounded shadows">
-                <div class="d-flex flex-wrap gap-3">
-                    <div class="box flex-grow-1">
-                        <label for="title" class="form-label">Título del trabajo <span class="text-danger">*</span></label>
-                        <input type="text" name="title" id="title" placeholder="Palabra clave, categoría o cargo" required maxlength="20" class="form-control"/>
-                    </div>
-                </div>
-            </form>
-        </section>
-        </div>
-       
-    )
-}
+const FiltroTrabajo = () => {
+  return (
+    <div>
+      <section class="job-filter bg-light">
+        <h1 class="heading mb-4">Buscar trabajos</h1>
+        <form
+          action=""
+          method="post"
+          class="p-4 bg-white border rounded shadows"
+        >
+          <div class="d-flex flex-wrap gap-3">
+            <div class="box flex-grow-1">
+              <label for="title" class="form-label">
+                Título del trabajo <span class="text-danger">*</span>
+              </label>
+              <input
+                type="text"
+                name="title"
+                id="title"
+                placeholder="Palabra clave, categoría o cargo"
+                required
+                maxlength="20"
+                class="form-control"
+              />
+            </div>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
+};
 export default FiltroTrabajo;
