1c3ff7da2eccd69f0f1736c3857ffdff13180145
Englis666 <eabarros2610@gmail.com>
2025-05-25 17:42:33 -0500

    UPDATE: Mejora del formato y espaciado en el componente Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-        <div className="credit text-center pt-4 border-top bg-white">&copy; Copyright @2024 by <span className="text-primary">Desarolladores de Gestor Plus</span> | Todos los derechos reservados</div>
+  return (
+    <footer className="footer">
+      <div className="credit text-center pt-4 border-top bg-white">
+        &copy; Copyright @2024 by{" "}
+        <span className="text-primary">Desarolladores de Gestor Plus</span> |
+        Todos los derechos reservados
+      </div>
     </footer>
-    );
-}
+  );
+};
 
 export default Footer;
