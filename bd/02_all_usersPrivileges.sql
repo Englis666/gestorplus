0d58557df1a9215fcdf81be38e236d3778fa37ed
Englis666 <eabarros2610@gmail.com>
2025-05-27 10:08:30 -0500

    Feature: Se crea el script SQL para la inclusión de privilegios de usuarios en la base de datos 'gestorplus'

---
+++ bd/02_all_usersPrivileges.sql
@@ -0,0 +1,4 @@
+SOURCE /var/www/gestorplus/bd/usuarios/usuario_admin.sql;
+SOURCE /var/www/gestorplus/bd/usuarios/usuario_app.sql;
+SOURCE /var/www/gestorplus/bd/usuarios/usuario_read.sql;
+SOURCE /var/www/gestorplus/bd/usuarios/usuario_root.sql;
