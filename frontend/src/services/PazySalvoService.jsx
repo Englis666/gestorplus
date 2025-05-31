6cbd3078cccccc35c593fd42df91a47f3c52fb51
Englis666 <eabarros2610@gmail.com>
2025-05-31 18:53:07 -0500

    feat: Implement PazySalvoService for managing Paz y Salvo operations

---
+++ frontend/src/services/PazySalvoService.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import API_URL from "../config";
+import { getCookie } from "../utils/Auth";
+
+export const obtenerPazYSalvos = async (rol) => {
+  const token = getCookie("auth_token");
+  if (!token) throw new Error("Token no encontrado");
+
+  let action;
+  switch (rol) {
+    case "1":
+    case "2":
+      action = "obtenerPazYSalvos";
+      break;
+    case "3":
+      action = "obtenerMiPazySalvo";
+      break;
+    default:
+      throw new Error("Rol no reconocido");
+  }
+
+  const response = await axios.get(API_URL, {
+    headers: { Authorization: `Bearer ${token}` },
+    params: { action },
+  });
+  return response.data?.Salvos || [];
+};
+
+export const generarPazYSalvo = async (form) => {
+  const token = getCookie("auth_token");
+  if (!token) throw new Error("Token no encontrado");
+
+  const response = await axios.post(
+    API_URL,
+    {
+      action: "generarPazYSalvo",
+      ...form,
+    },
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+  return response.data;
+};
