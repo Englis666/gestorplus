901ec0138f252e94dc975587c95a7124e8f1ce9c
Englis666 <eabarros2610@gmail.com>
2025-05-31 18:50:26 -0500

    fix: Correct error message in bootstrap.php and clean up dotenv loading code

--- backend/bootstrap.php
+++ backend/bootstrap.php
@@ -2,14 +2,13 @@
 
 use Core\Router;
 
-
-
 header("Content-Type: application/json; charset=UTF-8");
 
 require_once __DIR__ . '/vendor/autoload.php';
-// Cargar variables de entorno si usas .env
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-// $dotenv->load();
+
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $method = $_SERVER['REQUEST_METHOD'];
 $data = json_decode(file_get_contents('php://input'), true);
@@ -17,7 +16,7 @@ $action = $_GET['action'] ?? ($data['action'] ?? null);
 
 if (!$action) {
     http_response_code(400);
-    echo json_encode(['message' => 'No se recibió la acción.']);
+    echo json_encode(['message' => 'Se encontro el recurso.']);
     exit;
 }
 
