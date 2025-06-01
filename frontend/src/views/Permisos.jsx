a3e380eec48582ecc8390cfe4cf658d9e11682dc
Englis666 <eabarros2610@gmail.com>
2025-06-01 12:23:21 -0500

    feat: Refactor layout structure in Permisos component for improved styling and organization

--- frontend/src/views/Permisos.jsx
+++ frontend/src/views/Permisos.jsx
@@ -10,11 +10,14 @@ import TablaPermisos from "../componentsClosed/tables/TablaPermisos";
 const Permisos = () => {
   return (
     <div
-      className="bg-light min-vh-100"
+      className="bg-light main-layout"
       style={{ transition: "all 3s ease", display: "flex" }}
     >
       <NavbarClosed />
-      <div className="flex-grow-1 p-4" style={{ backgroundColor: "#ECF0F1" }}>
+      <div
+        className="flex-grow-1  main-content"
+        style={{ backgroundColor: "#ECF0F1" }}
+      >
         <TablaPermisos />
       </div>
     </div>
