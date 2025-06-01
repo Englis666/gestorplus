b3a65186cedea952ac92f8e0194de3975493eb73
Englis666 <eabarros2610@gmail.com>
2025-06-01 12:23:39 -0500

    feat: Refactor SistemaDeGestion component layout for improved readability and organization

--- frontend/src/views/SistemaDeGestion.jsx
+++ frontend/src/views/SistemaDeGestion.jsx
@@ -10,28 +10,36 @@ import FormularioParaAgregarSistemasDeGestion from "../componentsClosed/form/For
 import { useLocation } from "react-router-dom";
 
 const SistemaDeGestion = () => {
-    const location = useLocation();
-    const { num_doc, nombres, identrevista, idpostulacion } = location.state || {};
+  const location = useLocation();
+  const { num_doc, nombres, identrevista, idpostulacion } =
+    location.state || {};
 
-    const isFormDataAvailable = num_doc && nombres && identrevista && idpostulacion;
+  const isFormDataAvailable =
+    num_doc && nombres && identrevista && idpostulacion;
 
-    return (
-        <div className="bg-light min-vh-100" style={{ transition: "all 3s ease", display: "flex" }}>
-            <NavbarClosed />
-            <div className="flex-grow-1 p-4" style={{ background: "#ECF0F1" }}>
-                <TablaSistemaDeGestion />
-                
-                {isFormDataAvailable && (
-                    <FormularioParaAgregarSistemasDeGestion 
-                        num_doc={num_doc} 
-                        nombres={nombres} 
-                        identrevista={identrevista} 
-                        idpostulacion={idpostulacion} 
-                    />
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="bg-light min-vh-100 main-layout"
+      style={{ transition: "all 3s ease", display: "flex" }}
+    >
+      <NavbarClosed />
+      <div
+        className="flex-grow-1 p-4 main-content"
+        style={{ background: "#ECF0F1" }}
+      >
+        <TablaSistemaDeGestion />
+
+        {isFormDataAvailable && (
+          <FormularioParaAgregarSistemasDeGestion
+            num_doc={num_doc}
+            nombres={nombres}
+            identrevista={identrevista}
+            idpostulacion={idpostulacion}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SistemaDeGestion;
