597106348c0757dffbe65aecd9d09fa4826f9a19
Englis666 <eabarros2610@gmail.com>
2025-06-01 12:22:46 -0500

    feat: Add additional class names for layout styling in Entrevistas component

--- frontend/src/views/Entrevistas.jsx
+++ frontend/src/views/Entrevistas.jsx
@@ -3,18 +3,20 @@
  * Prohibida su copia, redistribución o uso sin autorización expresa de CodeAdvance.
  */
 
-import React from "react";
 import NavbarClosed from "../componentsClosed/Navbar";
 import TablaEntrevistas from "../componentsClosed/tables/TablaEntrevistas";
 
 const Entrevistas = () => {
   return (
     <div
-      className="bg-light min-vh-100"
+      className="bg-light min-vh-100 main-layout"
       style={{ transition: "all 3s ease", display: "flex" }}
     >
       <NavbarClosed />
-      <div className="flex-grow-1 p-4" style={{ backgroundColor: "#ECF0F1" }}>
+      <div
+        className="flex-grow-1 p-4 main-content"
+        style={{ backgroundColor: "#ECF0F1" }}
+      >
         <TablaEntrevistas />
       </div>
     </div>
