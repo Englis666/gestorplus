ac262307969050646ca1860fe891df8dfef7d2cb
Englis666 <eabarros2610@gmail.com>
2025-06-01 12:23:28 -0500

    feat: Enhance layout structure in Publicaciones component with additional class names

--- frontend/src/views/Publicaciones.jsx
+++ frontend/src/views/Publicaciones.jsx
@@ -10,11 +10,14 @@ import PublicacionesComp from "../componentsClosed/PublicacionesComp";
 const Publicaciones = () => {
   return (
     <div
-      className="bg-light min-vh-100"
+      className="bg-light min-vh-100 main-layout"
       style={{ transition: "all 3s ease", display: "flex" }}
     >
       <NavbarClosed />
-      <div className="flex-grow-1 p-4" style={{ backgroundColor: "#ECF0F1" }}>
+      <div
+        className="flex-grow-1 p-4 main-content"
+        style={{ backgroundColor: "#ECF0F1" }}
+      >
         <PublicacionesComp />
       </div>
     </div>
