a124d9977c9581935264ec449ae0606ffddce712
Englis666 <eabarros2610@gmail.com>
2025-06-01 12:23:24 -0500

    feat: Enhance layout structure in Postulaciones component with additional class names

--- frontend/src/views/Postulaciones.jsx
+++ frontend/src/views/Postulaciones.jsx
@@ -11,12 +11,12 @@ import TablaConvocatoriasAgrupadas from "../componentsClosed/tables/TablaConvoca
 const Postulaciones = () => {
   return (
     <div
-      className="bg-light min-vh-100"
+      className="bg-light min-vh-100 main-layout"
       style={{ transition: "all 0.3s ease", display: "flex" }}
     >
       <NavbarClosed />
       <div
-        className="flex-grow-1 p-4"
+        className="flex-grow-1 p-4 main-content"
         style={{
           backgroundColor: "#ECF0F1",
           display: "flex",
