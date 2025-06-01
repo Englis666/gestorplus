60acffc8b439af35f6b07f8aa7b967894d6e0f3b
Englis666 <eabarros2610@gmail.com>
2025-06-01 12:23:57 -0500

    feat: Enhance layout structure in Vacaciones component for improved organization and readability

--- frontend/src/views/Vacaciones.jsx
+++ frontend/src/views/Vacaciones.jsx
@@ -8,11 +8,14 @@ import TablaVacaciones from "../componentsClosed/tables/TablaVacaciones";
 const Vacaciones = () => {
   return (
     <div
-      className="bg-light min-vh-100"
+      className="bg-light min-vh-100 main-layout"
       style={{ transition: "all 3s ease", display: "flex" }}
     >
       <NavbarClosed />
-      <div className="flex-grow-1 p-4" style={{ backgroundColor: "#ECF0F1" }}>
+      <div
+        className="flex-grow-1 p-4 main-content"
+        style={{ backgroundColor: "#ECF0F1" }}
+      >
         <TablaVacaciones />
       </div>
     </div>
