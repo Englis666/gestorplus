0f6fa564b7145717f632a0f3d51bf58fe81e19ce
Englis666 <eabarros2610@gmail.com>
2025-06-01 12:22:53 -0500

    feat: Add additional class names for layout styling in PazYSalvos component

--- frontend/src/views/PazYSalvos.jsx
+++ frontend/src/views/PazYSalvos.jsx
@@ -3,18 +3,20 @@
  * Prohibida su copia, redistribución o uso sin autorización expresa de CodeAdvance.
  */
 
-import React from "react";
 import NavbarClosed from "../componentsClosed/Navbar";
 import TablaPazYSalvo from "../componentsClosed/tables/TablaPazYSalvo";
 
 const PazySalvo = () => {
   return (
     <div
-      className="bg-light min-vh-100"
+      className="bg-light min-vh-100 main-layout"
       style={{ transition: "all 3s ease", display: "flex" }}
     >
       <NavbarClosed />
-      <div className="flex-grow-1 p-4" style={{ backgroundColor: "#ECF0F1" }}>
+      <div
+        className="flex-grow-1 p-4 main-content"
+        style={{ backgroundColor: "#ECF0F1" }}
+      >
         <TablaPazYSalvo />
       </div>
     </div>
